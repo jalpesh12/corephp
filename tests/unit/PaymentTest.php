6b41bd8017fc99d3ffcd873d966e85ee4a816302
Jalpesh <jalpesh.patel1@gmail.com>
2021-07-27 21:27:46 +1000

    Added test case with full test coverage

--- tests/unit/PaymentTest.php
+++ tests/unit/PaymentTest.php
@@ -6,7 +6,6 @@ use TestApp\Payment\Onlineshop;
 
 class PaymentTest extends TestCase 
 {
-
     public function testChargeConfirmed() {
         $payment = $this->getMockBuilder(Payment::class)
                         ->setMethods(['authorize'])
@@ -19,8 +18,25 @@ class PaymentTest extends TestCase
         $shop = new Onlineshop();
         $response = $shop->charge($payment);
         $this->assertTrue($response);
+    }
 
+    public function testChargeNotConfirmed() {
+        $payment = $this->getMockBuilder(Payment::class)
+                        ->setMethods(['authorize'])
+                        ->getMock();
 
+        $payment->expects($this->once())
+                ->method('authorize')
+                ->will($this->returnValue('CONFIRMEDs'));
+        
+        $shop = new Onlineshop();
+        $response = $shop->charge($payment);
+        $this->assertFalse($response);
+    }
+
+    public function testAuthorize() {
+        $payment = new Payment();
+        $this->assertEquals('CONFIRMED', $payment->authorize());
     }
 
 }
